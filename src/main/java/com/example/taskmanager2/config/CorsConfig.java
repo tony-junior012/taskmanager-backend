96825195db273f471e5f072c5db5a823badf6742
tony-junior012 <tj.yokota@gmail.com>
2025-03-21 15:50:20 -0300

    initial commit

--- src/main/java/com/example/taskmanager2/config/CorsConfig.java
+++ src/main/java/com/example/taskmanager2/config/CorsConfig.java
@@ -4,19 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permite todos os endpoints
-                        .allowedOrigins("*") // Permite apenas o frontend na porta 3000
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
-                        .allowedHeaders("*") // Permite todos os cabeçalhos
-                        .allowCredentials(true); // Permite envio de cookies, se necessário
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todos os endpoints
+                .allowedOrigins("*") // Permite apenas o frontend na porta 3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite envio de cookies, se necessário
     }
 
 }
